09efa8b0c3e1f9842741816165b2503853fa7748
unknown <Administrador@.(none)>
2011-01-29 16:42:28 +0100

    prueba

--- grails-app/domain/cinema/Boc.groovy
+++ grails-app/domain/cinema/Boc.groovy
@@ -8,7 +8,7 @@ class Boc {
     String accion
     static belongsTo = [exhibidor:Exhibidor]
     static constraints = {
-		accion(nullable:false, inList:["DEVOLUCION2","PEDIDO"])
+		accion(nullable:false, inList:["DEVOLUCION5","PEDIDO"])
 		serie(nullable:false, matches:"[A..Z]")
 		
     }
